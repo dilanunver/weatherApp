f56da57cd4d5f52cad2facad01e5286e151270b8
dilanunver <dunver33@gmail.com>
2021-05-11 18:20:13 +0300

    adding description and pictures

--- src/App.js
+++ src/App.js
@@ -3,37 +3,63 @@ import './App.css';
 import data, { getCityDataForSelect } from '../src/data'
 import Select from 'react-select'
 
+
 // const url = 'http://api.openweathermap.org/data/2.5/weather?q=Mersin&appid=84f9b04fac44b3d3edfcb86811e994b4'
 
 function App() {
   const [cities, setCities] = useState(data)
   const [selectedCity, setSelectedCity] = useState('')
   const [userSelectedCity, setUserSelectedCity] = useState('Ankara')
-  const [temperature, setTemperature] = useState('')
+  const [temperature, setTemperature] = useState('0')
+  const [desc, setDesc] = useState('clear')
+  const [date, setDate] = useState(new Date())
 
+
+  useEffect(() => {
+    const timer = setInterval(() => setDate(new Date()), 1000);
+    return function cleanup() {
+      clearInterval(timer)
+    }
+  })
 
   const handleSelectCity = async () => {
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${userSelectedCity}&appid=84f9b04fac44b3d3edfcb86811e994b4`
     const response = await fetch(url)
     const result = await response.json()
-    console.log(result.main.temp)
     setSelectedCity(result.name);
     let converter = parseFloat(result.main.temp);
     let tempConverter = (converter - 273.15).toFixed();
-    console.log(tempConverter)
     setTemperature(tempConverter)
-    // console.log(selectedCity)
+    console.log(result.weather)
+    setDesc(result.weather[0].main)
+
   }
 
 
+
+
   return (
-    <div className='select-button'>
-      <Select options={getCityDataForSelect}
-        onChange={e => setUserSelectedCity(e.value)}
-        className='select'
-        placeholder='select weather'></Select>
-      <button onClick={handleSelectCity}>Search</button>
-      <h5>{selectedCity} {temperature} °C </h5>
+    <div className='all-info'>
+      <div className='select'>
+        <Select options={getCityDataForSelect}
+          onChange={e => setUserSelectedCity(e.value)}
+          className='select-select'
+          placeholder='select weather'></Select>
+        <button className='select-button' onClick={handleSelectCity}>Search</button>
+      </div>
+      <div className='info'>
+        <h3 className='city'>{selectedCity} </h3>
+        <div className='time-date'>
+          <p className='time'>{date.toLocaleTimeString()}</p>
+          <p >{date.toLocaleDateString()}</p>
+        </div>
+        <p className='desc'>{desc}</p>
+        <p className='temp'>{temperature}° </p>
+
+
+
+      </div>
+
     </div>
   );
 }
